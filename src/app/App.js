c2fe218047be4723e140967897f3286f6ebd5395
ydubler <ydubler@gmail.com>
2020-07-07 13:45:15 -0600

    Adding mobile support

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import "../../public/css/App.css";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
+import "../../public/css/App.css"; // CSS
 
 function App() {
   // State
@@ -7,15 +13,28 @@ function App() {
 
   return (
     <>
-      <div className="portfolio">
-        <div class="portfolio-text">Portfolio</div>
-      </div>
-      <div className="navbar">
-        <div class="navbar-option">WEB APPLICATIONS</div>
-        <div class="navbar-option">COMPONENTS</div>
-      </div>
-      {/* <img src="/public/images/coffee1.jpg"></img> */}
-      <div className="one font-lobster">{greeting}</div>
+      <BrowserView>
+        <div className="portfolio">
+          <div class="portfolio-text">Portfolio</div>
+        </div>
+        <div className="navbar">
+          <div class="navbar-option-browser">WEB APPLICATIONS</div>
+          <div class="navbar-option-browser">COMPONENTS</div>
+        </div>
+        {/* <img src="/public/images/coffee1.jpg"></img> */}
+        <div className="one font-lobster">{greeting}</div>
+      </BrowserView>
+      <MobileView>
+        <div className="portfolio">
+          <div class="portfolio-text">Portfolio</div>
+        </div>
+        <div className="navbar">
+          <div class="navbar-option-mobile">WEB APPLICATIONS</div>
+          <div class="navbar-option-mobile">COMPONENTS</div>
+        </div>
+        {/* <img src="/public/images/coffee1.jpg"></img> */}
+        <div className="one font-lobster">{greeting}</div>
+      </MobileView>
     </>
   );
 }
