2d73e9aa828033fc0b38ada8c9544bb365b001ac
ydubler <switchgoof@gmail.com>
2020-07-13 19:13:28 -0600

    Updates

--- src/app/App.js
+++ src/app/App.js
@@ -134,7 +134,9 @@ function App() {
             <>
               <div className="main-content-item">
                 <div className="main-content-item-br"></div>
-                <div className="main-content-item-title">Portfolio Website</div>
+                <div className="main-content-item-title">
+                  Portfolio Website (This Website)
+                </div>
                 <br />
                 <div className="main-content-item-tech-major">REACT</div>
                 <div className="main-content-item-tech-major">NODE</div>
@@ -176,13 +178,20 @@ function App() {
                 <br />
                 <div className="main-content-item-feature">RESPONSIVE</div>
                 <div className="main-content-item-feature">
+                  HTTP REQUEST/RESPONSE
+                </div>
+                <div className="main-content-item-feature">SQL QUERIES</div>
+                <div className="main-content-item-feature">
                   PASSWORD HASHING
+                </div>
+                <div className="main-content-item-feature">
+                  AUTHENTICATION/AUTHORIZATION
                 </div>
                 <div className="main-content-item-feature">LOGIN SYSTEM</div>
                 <div className="main-content-item-feature">USER SESSIONS</div>
                 <div className="main-content-item-feature">ADMIN CONTROLS</div>
                 <div className="main-content-item-feature">
-                  INVENTORY MANAGEMENT SYSTEM
+                  INVENTORY MANAGEMENT
                 </div>
                 <div className="main-content-item-feature">ONLINE STORE</div>
                 <br />
@@ -191,8 +200,10 @@ function App() {
                   Technology Group located in Missoula, Montana. It has numerous
                   features including a login system, password hashing, user
                   sessions, administrative controls, an inventory management
-                  system, and an online store. I do not own this project so I
-                  can not provide any links to it or source code.
+                  system, and an online store.
+                  <br />
+                  <br />I do not own this project so I can not provide any links
+                  to it or source code.
                 </div>
               </div>
               <div className="main-content-item-br"></div>
@@ -208,18 +219,27 @@ function App() {
                 <div className="main-content-item-tech-minor">WEBPACK</div>
                 <div className="main-content-item-tech-minor">SVG</div>
                 <br />
-                <div className="main-content-item-tech-feature">
-                  SQL QUERIES
-                </div>
-                <div className="main-content-item-tech-feature">
+                <div className="main-content-item-feature">SQL QUERIES</div>
+                <div className="main-content-item-feature">
                   DATA VISUALIZATION
                 </div>
                 <br />
+                <div className="main-content-item-github">
+                  <a
+                    target="_blank"
+                    href="https://github.com/ydubler/game-stats"
+                  >
+                    View On Github
+                  </a>
+                </div>
                 <div className="main-content-item-descr">
                   This web-application was inspired by my board game groups
                   fascination with the Game of Thrones board game. It allows the
                   user to save a game session into the database and then loads
                   that data to display numerous graphs about the game.
+                  <br />
+                  <br />
+                  This project is not yet deployed to a server.
                 </div>
               </div>
               <div className="main-content-item-br"></div>
